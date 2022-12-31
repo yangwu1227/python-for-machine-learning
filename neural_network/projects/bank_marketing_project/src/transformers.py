f12503496446ec3932f7f082d6e86006907fa519
YangWu1227 <yangwu2020@gmail.com>
2022-12-31 01:07:58 -0500

    update: banking project using neural network

---
+++ neural_network/projects/bank_marketing_project/src/transformers.py
@@ -0,0 +1,60 @@
+# ---------------------------------- Imports --------------------------------- #
+
+import numpy as np
+import pandas as pd
+from re import sub
+from feature_engine.encoding import OneHotEncoder, CountFrequencyEncoder
+from sklearn.preprocessing import FunctionTransformer
+from sklearn.base import BaseEstimator, TransformerMixin
+
+# ---------------------------- Numerical features ---------------------------- #
+
+
+def NumTransformer(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Group by aggregation of numerical columns based on characteristics--- age, job, education, contact, marital, housing, day_of_week, month.
+    """
+    # Define list of numerical columns to aggregate
+    num_col = ['campaign',
+               'pdays',
+               'previous',
+               'emp_var_rate',
+               'cons_price_idx',
+               'cons_conf_idx',
+               'euribor3m',
+               'nr_employed']
+
+    for group in ['age', 'job', 'marital', 'contact', 'education', 'housing', 'day_of_week', 'month', 'poutcome']:
+        for agg_func in ['mean', 'median', 'min', 'max', 'last']:
+            df[[col + f'_{agg_func}_by_{group}' for col in num_col]
+               ] = df.groupby(group)[num_col].transform(agg_func, engine='cython')
+
+    return df
+
+# ------------------ Convert columns to categorical datatype ----------------- #
+
+
+def CatTransformer(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Function to convert columns to categorical datatype.
+    """
+    # Convert categorical features to 'category' datatype
+    cat_col = ['age', 'job', 'marital', 'education', 'default',
+               'housing', 'loan', 'contact', 'month', 'day_of_week', 'poutcome']
+
+    for col in cat_col:
+        df[col] = df[col].astype('category')
+
+    return df
+
+# --------------------------- Restore column names --------------------------- #
+
+
+def RestoreColNames(df: pd.DataFrame) -> pd.DataFrame:
+    """
+    Function to restore column names.
+    """
+    # Restore column names
+    df.columns = [sub(r'scaler__|remainder__', '', col) for col in df.columns]
+
+    return df
---
+++ neural_network/projects/bank_marketing_project/src/utils.py
@@ -0,0 +1,81 @@
+# ---------------------------------- Imports --------------------------------- #
+
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.metrics import confusion_matrix, roc_curve, precision_recall_curve, log_loss, average_precision_score, recall_score, precision_score
+import numpy as np
+import tensorflow as tf
+
+# ------------------------------- Plot metrics ------------------------------- #
+
+
+def plot_metrics(history):
+    metrics = ['loss', 'prc', 'precision', 'recall']
+    colors = plt.rcParams['axes.prop_cycle'].by_key()['color']
+    plt.rcParams['figure.figsize'] = (17, 15)
+    for n, metric in enumerate(metrics):
+        name = metric.replace("_", " ").capitalize()
+        plt.subplot(2, 2, n+1)
+        plt.plot(history.epoch,
+                 history.history[metric], color=colors[0], label='Train')
+        plt.plot(history.epoch, history.history['val_'+metric],
+                 color=colors[0], linestyle="--", label='Val')
+        plt.xlabel('Epoch')
+        plt.ylabel(name)
+        if metric == 'loss':
+            plt.ylim([0, plt.ylim()[1]])
+        elif metric == 'auc':
+            plt.ylim([0.8, 1])
+        else:
+            plt.ylim([0, 1])
+
+        plt.legend()
+
+# --------------------------- Plot confusion matrix -------------------------- #
+
+
+def plot_cm(labels, predictions, p=0.5):
+    cm = confusion_matrix(labels, predictions > p)
+    plt.rcParams['figure.figsize'] = (17, 15)
+    plt.figure(figsize=(5, 5))
+    sns.heatmap(cm, annot=True, fmt='d')
+    plt.title('Confusion matrix @{:.2f}'.format(p))
+    plt.ylabel('Actual label')
+    plt.xlabel('Predicted label')
+
+    print(
+        'Non-subscriber correctly identified as non-subscriber (True Negatives): ', cm[0][0])
+    print(
+        'Non-subscriber incorrectly identified as subscriber (False Positives): ', cm[0][1])
+    print(
+        'Subscriber incorrectly identified as non-subscriber (False Negatives): ', cm[1][0])
+    print(
+        'Subscriber correctly identified as subscribers (True Positives): ', cm[1][1])
+    print('Total subscribers identified: ', np.sum(cm[1]))
+
+# --------------------------------- Plot ROC --------------------------------- #
+
+
+def plot_roc(name, labels, predictions, **kwargs):
+    fp, tp, _ = roc_curve(labels, predictions)
+    plt.rcParams['figure.figsize'] = (17, 15)
+    plt.plot(100*fp, 100*tp, label=name, linewidth=2, **kwargs)
+    plt.plot([0, 100], [0, 100], "k--", label="chance level (AUC = 0.5)")
+    plt.xlabel('False positives [%]')
+    plt.ylabel('True positives [%]')
+    plt.grid(True)
+    ax = plt.gca()
+    ax.set_aspect('equal')
+
+# --------------------------------- Plot PRC --------------------------------- #
+
+
+def plot_prc(name, labels, predictions, **kwargs):
+    precision, recall, _ = precision_recall_curve(labels, predictions)
+
+    plt.plot(precision, recall, label=name, linewidth=2, **kwargs)
+    plt.xlabel('Precision')
+    plt.ylabel('Recall')
+    plt.grid(True)
+    ax = plt.gca()
+    ax.set_aspect('equal')
