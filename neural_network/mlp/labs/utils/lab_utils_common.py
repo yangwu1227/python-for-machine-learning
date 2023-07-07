57de614f26811f5bc869cf0ceb0425311895deea
YangWu1227 <yangwu2020@gmail.com>
2023-07-07 03:20:56 -0500

    chore: restructure neural network directory

---
+++ neural_network/mlp/labs/utils/lab_utils_common.py
@@ -0,0 +1,364 @@
+"""
+Lab_utils_common
+Contains common routines and variable definitions
+Used by all the labs in this week.
+By contrast, specific, large plotting routines will be in separate files
+And are generally imported into the week where they are used.
+Those files will import this file
+"""
+import copy
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.patches import FancyArrowPatch
+from ipywidgets import Output
+from matplotlib.widgets import Button, CheckButtons
+
+np.set_printoptions(precision=2)
+
+dlc = {
+    'dlblue': '#0096ff',
+    'dlorange': '#FF9300',
+    'dldarkred': '#C00000',
+    'dlmagenta': '#FF40FF',
+    'dlpurple': '#7030A0',
+    'dldarkblue': '#0D5BDC'
+}
+
+dlblue = '#0096ff'
+dlorange = '#FF9300'
+dldarkred = '#C00000'
+dlmagenta = '#FF40FF'
+dlpurple = '#7030A0'
+dldarkblue = '#0D5BDC'
+dlcolors = [dlblue, dlorange, dldarkred, dlmagenta, dlpurple]
+
+plt.style.use('utils/deeplearning.mplstyle')
+
+
+def sigmoid(z):
+    """
+    Compute the sigmoid of z
+
+    Parameters
+    ----------
+    z : array_like
+        A scalar or numpy array of any size.
+
+    Returns
+    -------
+    g : array_like
+        sigmoid(z)
+    """
+    z = np.clip(z, -500, 500)  # Protect against overflow
+    g = 1.0 / (1.0 + np.exp(-z))
+
+    return g
+
+
+# Regression Routines
+
+def predict_logistic(X, w, b):
+    """Performs prediction."""
+    return sigmoid(X @ w + b)
+
+
+def predict_linear(X, w, b):
+    """Performs prediction."""
+    return X @ w + b
+
+
+def compute_cost_logistic(X, y, w, b, lambda_=0, safe=False):
+    """
+    Computes cost using logistic loss, non-matrix version
+
+    Args:
+      X (ndarray): Shape (m,n)  matrix of examples with n features
+      y (ndarray): Shape (m,)   target values
+      w (ndarray): Shape (n,)   parameters for prediction
+      b (scalar):               parameter  for prediction
+      lambda_ : (scalar, float) Controls amount of regularization, 0 = no regularization
+      safe : (boolean)          True-selects under/overflow safe algorithm
+    Returns:
+      cost (scalar): cost
+    """
+
+    m, n = X.shape
+    cost = 0.0
+    for i in range(m):
+        z_i = np.dot(X[i], w) + b  # (n,)(n,) or (n,) ()
+        if safe:  # Avoids overflows
+            cost += -(y[i] * z_i) + log_1pexp(z_i)
+        else:
+            f_wb_i = sigmoid(z_i)  # (n,)
+            cost += -y[i] * np.log(f_wb_i) - (1 - y[i]) * np.log(1 - f_wb_i)  # Scalar
+    cost = cost / m
+
+    reg_cost = 0
+    if lambda_ != 0:
+        for j in range(n):
+            reg_cost += (w[j] ** 2)  # Scalar
+        reg_cost = (lambda_ / (2 * m)) * reg_cost
+
+    return cost + reg_cost
+
+
+def log_1pexp(x, maximum=20):
+    """Approximate log(1+exp^x)
+    https://stats.stackexchange.com/questions/475589/numerical-computation-of-cross-entropy-in-practice
+    Args:
+    x   : (ndarray Shape (n,1) or (n,)  input
+    out : (ndarray Shape matches x      output ~= np.log(1+exp(x))
+    """
+
+    out = np.zeros_like(x, dtype=float)
+    i = x <= maximum
+    ni = np.logical_not(i)
+
+    out[i] = np.log(1 + np.exp(x[i]))
+    out[ni] = x[ni]
+    return out
+
+
+def compute_cost_matrix(X, y, w, b, logistic=False, lambda_=0, safe=True):
+    """
+    Computes the cost using  using matrices
+    Args:
+      X : (ndarray, Shape (m,n))          matrix of examples
+      y : (ndarray  Shape (m,) or (m,1))  target value of each example
+      w : (ndarray  Shape (n,) or (n,1))  Values of parameter(s) of the model
+      b : (scalar )                       Values of parameter of the model
+      verbose : (Boolean) If true, print out intermediate value f_wb
+    Returns:
+      total_cost: (scalar)                cost
+    """
+    m = X.shape[0]
+    y = y.reshape(-1, 1)  # Ensure 2D
+    w = w.reshape(-1, 1)  # Ensure 2D
+    if logistic:
+        if safe:  # Safe from overflow
+            z = X @ w + b  # (m,n)(n,1)=(m,1)
+            cost = -(y * z) + log_1pexp(z)
+            cost = np.sum(cost) / m  # (scalar)
+        else:
+            f = sigmoid(X @ w + b)  # (m,n)(n,1) = (m,1)
+            cost = (1 / m) * (np.dot(-y.T, np.log(f)) - np.dot((1 - y).T, np.log(1 - f)))  # (1,m)(m,1) = (1,1)
+            cost = cost[0, 0]  # Scalar
+    else:
+        f = X @ w + b  # (m,n)(n,1) = (m,1)
+        cost = (1 / (2 * m)) * np.sum((f - y) ** 2)  # Scalar
+
+    reg_cost = (lambda_ / (2 * m)) * np.sum(w ** 2)  # Scalar
+
+    total_cost = cost + reg_cost  # Scalar
+
+    return total_cost  # Scalar
+
+def compute_gradient_matrix(X, y, w, b, logistic=False, lambda_=0):
+    """
+    Computes the gradient using matrices.
+
+    Args:
+      X : (ndarray, Shape (m,n))          Matrix of examples.
+      y : (ndarray  Shape (m,) or (m,1))  Target value of each example.
+      w : (ndarray  Shape (n,) or (n,1))  Values of parameters of the model.
+      b : (scalar )                       Values of parameter of the model.
+      logistic: (boolean)                 Linear if false, logistic if true.
+      lambda_:  (float)                   Applies regularization if non-zero.
+
+    Returns:
+      dj_dw: (array_like Shape (n,1))     The gradient of the cost w.r.t. the parameters w.
+      dj_db: (scalar)                     The gradient of the cost w.r.t. the parameter b.
+    """
+    m = X.shape[0]
+    y = y.reshape(-1, 1)  # Ensure 2D.
+    w = w.reshape(-1, 1)  # Ensure 2D.
+
+    f_wb = sigmoid(X @ w + b) if logistic else X @ w + b  # (m,n)(n,1) = (m,1).
+    err = f_wb - y  # (m,1).
+    dj_dw = (1 / m) * (X.T @ err)  # (n,m)(m,1) = (n,1).
+    dj_db = (1 / m) * np.sum(err)  # Scalar.
+
+    dj_dw += (lambda_ / m) * w  # Regularize. # (n,1).
+
+    return dj_db, dj_dw  # Scalar, (n,1).
+
+
+def gradient_descent(X, y, w_in, b_in, alpha, num_iters, logistic=False, lambda_=0, verbose=True, Trace=True):
+    """
+    Performs batch gradient descent to learn theta. Updates theta by taking
+    num_iters gradient steps with learning rate alpha.
+
+    Args:
+      X (ndarray):    Shape (m,n)         Matrix of examples.
+      y (ndarray):    Shape (m,) or (m,1) Target value of each example.
+      w_in (ndarray): Shape (n,) or (n,1) Initial values of parameters of the model.
+      b_in (scalar):                      Initial value of parameter of the model.
+      logistic: (boolean)                 Linear if false, logistic if true.
+      lambda_:  (float)                   Applies regularization if non-zero.
+      alpha (float):                      Learning rate.
+      num_iters (int):                    Number of iterations to run gradient descent.
+
+    Returns:
+      w (ndarray): Shape (n,) or (n,1)    Updated values of parameters; matches incoming shape.
+      b (scalar):                         Updated value of parameter.
+    """
+    # An array to store cost J and w's at each iteration primarily for graphing later.
+    J_history = []
+    w = copy.deepcopy(w_in)  # Avoid modifying global w within function.
+    b = b_in
+    w = w.reshape(-1, 1)  # Prep for matrix operations.
+    y = y.reshape(-1, 1)
+    last_cost = np.Inf
+
+    for i in range(num_iters):
+        # Calculate the gradient and update the parameters.
+        dj_db, dj_dw = compute_gradient_matrix(X, y, w, b, logistic, lambda_)
+
+        # Update Parameters using w, b, alpha and gradient.
+        w = w - alpha * dj_dw
+        b = b - alpha * dj_db
+
+        # Save cost J at each iteration.
+        ccost = compute_cost_matrix(X, y, w, b, logistic, lambda_)
+        if Trace and i < 100000:  # Prevent resource exhaustion.
+            J_history.append(ccost)
+
+        # Print cost every at intervals 10 times or as many iterations if < 10.
+        if i % math.ceil(num_iters / 10) == 0:
+            if verbose: 
+                print(f'Iteration {i:4d}: Cost {ccost}   ')
+            if verbose == 2: 
+                print(f'dj_db, dj_dw = {dj_db: 0.3f}, {dj_dw.reshape(-1)}')
+
+            if ccost == last_cost:
+                alpha = alpha / 10
+                print(f' alpha now {alpha}')
+            last_cost = ccost
+
+    return w.reshape(w_in.shape), b, J_history  # Return final w,b and J history for graphing.
+
+
+def zscore_normalize_features(X):
+    """
+    Computes  X, zcore normalized by column.
+
+    Args:
+      X (ndarray): Shape (m,n) Input data, m examples, n features.
+
+    Returns:
+      X_norm (ndarray): Shape (m,n)  Input normalized by column.
+      mu (ndarray):     Shape (n,)   Mean of each feature.
+      sigma (ndarray):  Shape (n,)   Standard deviation of each feature.
+    """
+    # Find the mean of each column/feature.
+    mu = np.mean(X, axis=0)  # Mu will have shape (n,).
+    # Find the standard deviation of each column/feature.
+    sigma = np.std(X, axis=0)  # Sigma will have shape (n,).
+    # Element-wise, subtract mu for that column from each example, divide by std for that column.
+    X_norm = (X - mu) / sigma
+
+    return X_norm, mu, sigma
+
+
+def plot_data(X, y, ax, pos_label='y=1', neg_label='y=0', s=80, loc='best'):
+    """Plots logistic data with two axis."""
+    # Find Indices of Positive and Negative Examples.
+    pos = y == 1
+    neg = y == 0
+    pos = pos.reshape(-1,)  # Work with 1D or 1D y vectors.
+    neg = neg.reshape(-1,)
+
+    # Plot examples.
+    ax.scatter(X[pos, 0], X[pos, 1], marker='x', s=s, c='red', label=pos_label)
+    ax.scatter(X[neg, 0], X[neg, 1], marker='o', s=s, label=neg_label, facecolors='none', edgecolors=dlblue, lw=3)
+    ax.legend(loc=loc)
+
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
+    
+def plt_tumor_data(x, y, ax):
+    """Plots tumor data on one axis."""
+    pos = y == 1
+    neg = y == 0
+
+    ax.scatter(x[pos], y[pos], marker='x', s=80, c='red', label='Malignant')
+    ax.scatter(x[neg], y[neg], marker='o', s=100, label='Benign', facecolors='none', edgecolors=dlblue, lw=3)
+    ax.set_ylim(-0.175, 1.1)
+    ax.set_ylabel('Y')
+    ax.set_xlabel('Tumor Size')
+    ax.set_title('Logistic Regression on Categorical Data')
+
+    ax.figure.canvas.toolbar_visible = False
+    ax.figure.canvas.header_visible = False
+    ax.figure.canvas.footer_visible = False
+
+
+def draw_vthresh(ax, x):
+    """Draws a threshold."""
+    ylim = ax.get_ylim()
+    xlim = ax.get_xlim()
+    ax.fill_between([xlim[0], x], [ylim[1], ylim[1]], alpha=0.2, color=dlblue)
+    ax.fill_between([x, xlim[1]], [ylim[1], ylim[1]], alpha=0.2, color=dldarkred)
+    ax.annotate('Z >= 0', xy=[x, 0.5], xycoords='data', xytext=[30, 5], textcoords='offset points')
+    d = FancyArrowPatch(
+        posA=(x, 0.5), posB=(x+3, 0.5), color=dldarkred,
+        arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
+    )
+    ax.add_artist(d)
+    ax.annotate('Z < 0', xy=[x, 0.5], xycoords='data', xytext=[-50, 5], textcoords='offset points', ha='left')
+    f = FancyArrowPatch(
+        posA=(x, 0.5), posB=(x-3, 0.5), color=dlblue,
+        arrowstyle='simple, head_width=5, head_length=10, tail_width=0.0',
+    )
+    ax.add_artist(f)
+
+class ButtonManager:
+    '''Handles some missing features of matplotlib check buttons.
+    
+    On init:
+        Creates button, links to button_click routine,
+        calls call_on_click with active index and firsttime=True.
+    On click:
+        Maintains single button on state, calls call_on_click.
+    '''
+
+    def __init__(self, fig, dim, labels, init, call_on_click):
+        '''
+        Initializes the ButtonManager.
+
+        Parameters
+        ----------
+        fig : matplotlib.figure.Figure
+            The figure object.
+        dim : list
+            The dimensions [leftbottom_x, bottom_y, width, height].
+        labels : list
+            The labels for the buttons, e.g., ['1', '2', '3', '4', '5', '6'].
+        init : list
+            The initial state of the buttons, e.g., [True, False, False, False, False, False].
+        call_on_click : callable
+            The function to call when a button is clicked.
+        '''
+        self.fig = fig
+        self.ax = plt.axes(dim)  # lx, by, w, h
+        self.init_state = init
+        self.call_on_click = call_on_click
+        self.button = CheckButtons(self.ax, labels, init)
+        self.button.on_clicked(self.button_click)
+        self.status = self.button.get_status()
+        self.call_on_click(self.status.index(True), firsttime=True)
+
+    def reinit(self):
+        '''Reinitializes the button manager to its initial state.'''
+        self.status = self.init_state
+        self.button.set_active(self.status.index(True))  # turn off old, will trigger update and set to status
+
+    def button_click(self, event):
+        '''Maintains one-on state. If on-button is clicked, will process correctly.'''
+        self.button.eventson = False
+        self.button.set_active(self.status.index(True))  # turn off old or reenable if same
+        self.button.eventson = True
+        self.status = self.button.get_status()
+        self.call_on_click(self.status.index(True))
---
+++ neural_network/techniques_frameworks/back_propagation_regularization/lab_utils_backprop.py
@@ -0,0 +1,465 @@
+from sympy import *
+import numpy as np
+import re
+
+import matplotlib.pyplot as plt
+from matplotlib.widgets import TextBox
+from matplotlib.widgets import Button
+import ipywidgets as widgets
+
+
+def widgvis(fig):
+    '''Hides the toolbar, header, and footer of a figure.'''
+    fig.canvas.toolbar_visible = False
+    fig.canvas.header_visible = False
+    fig.canvas.footer_visible = False
+
+
+def between(a, b, x):
+    '''Determines if a point x is between a and b. a may be greater or less than b.
+
+    Parameters
+    ----------
+    a : float
+        The first boundary.
+    b : float
+        The second boundary.
+    x : float
+        The point to check.
+
+    Returns
+    -------
+    bool
+        True if x is between a and b, False otherwise.
+    '''
+    if a > b:
+        return b <= x <= a
+    if b > a:
+        return a <= x <= b
+
+
+def near(pt, alist, dist=15):
+    '''Checks if a point is near any point in a list.
+
+    Parameters
+    ----------
+    pt : tuple
+        The x, y coordinates of the point.
+    alist : list
+        The list of points to check.
+    dist : float, optional
+        The distance to consider as "near", by default 15.
+
+    Returns
+    -------
+    tuple
+        A tuple (bool, object), where the bool is True if the point is near any point in the list, and the object is the point in the list that is near. If the point is not near any point in the list, returns (False, None).
+    '''
+    for a in alist:
+        x, y = a.ao.get_position()  # Use (bot left, bot right) data coords, not relative
+        x = x - 5  
+        y = y + 2.5
+        if 0 < (pt[0] - x) < 25 and 0 < (y - pt[1]) < 25:
+            return (True, a)
+    return (False, None)
+
+
+def inboxes(pt, boxlist):
+    '''Checks if a point is within any box in a list.
+
+    Parameters
+    ----------
+    pt : tuple
+        The x, y coordinates of the point.
+    boxlist : list
+        The list of boxes to check.
+
+    Returns
+    -------
+    tuple
+        A tuple (bool, object), where the bool is True if the point is within any box in the list, and the object is the box in the list that contains the point. If the point is not within any box in the list, returns (False, None).
+    '''
+    for b in boxlist:
+        if b.inbox(pt):
+            return (True, b)
+    return (False, None)
+
+class avalue:
+    '''One of the values on the figure that can be filled in.'''
+
+    def __init__(self, value, pt, cl):
+        '''Initializes the AValue.
+
+        Parameters
+        ----------
+        value : float
+            The value.
+        pt : tuple
+            The x, y coordinates of the value.
+        cl : str
+            The color of the value.
+        '''
+        self.value = value
+        self.cl = cl  # color
+        self.pt = pt  # point
+    
+    def add_anote(self, ax):
+        '''Adds an annotation to the value.
+
+        Parameters
+        ----------
+        ax : Axes
+            The axes for the annotation.
+        '''
+        self.ax = ax
+        self.ao = self.ax.annotate('?', self.pt, c=self.cl, fontsize='x-small')
+
+
+class astring:
+    '''A string that can be set visible or invisible.'''
+
+    def __init__(self, ax, string, pt, cl):
+        '''Initializes the AString.
+
+        Parameters
+        ----------
+        ax : Axes
+            The axes for the string.
+        string : str
+            The string.
+        pt : tuple
+            The x, y coordinates of the string.
+        cl : str
+            The color of the string.
+        '''
+        self.string = string
+        self.cl = cl  # color
+        self.pt = pt  # point
+        self.ax = ax
+        self.ao = self.ax.annotate(self.string, self.pt, c='white', fontsize='x-small')
+    
+    def astring_visible(self):
+        '''Makes the string visible.'''
+        self.ao.set_color(self.cl)
+
+    def astring_invisible(self):
+        '''Makes the string invisible.'''
+        self.ao.set_color('white')
+
+
+class ABox:
+    '''One of the boxes in the graph that has a value.'''
+
+    def __init__(self, ax, value, left, bottom, right, top, anpt, cl, adj_anote_obj):
+        '''Initializes the ABox.
+
+        Parameters
+        ----------
+        ax : Axes
+            The axes for the box.
+        value : float
+            The correct value for annotation.
+        left : float
+            The left coordinate of the box.
+        bottom : float
+            The bottom coordinate of the box.
+        right : float
+            The right coordinate of the box.
+        top : float
+            The top coordinate of the box.
+        anpt : tuple
+            The x, y coordinates where expression should be listed.
+        cl : str
+            The color of the box.
+        adj_anote_obj : object
+            The secondary text for marking edges or None.
+        '''
+        self.ax = ax
+        self.value = value  # Correct value for annotation
+        self.left = left
+        self.right = right 
+        self.bottom = bottom
+        self.top = top
+        self.anpt = anpt  # X, y where expression should be listed
+        self.cl = cl
+        self.ao = self.ax.annotate('?', self.anpt, c=self.cl, fontsize='x-small')
+        self.astr = adj_anote_obj  # Secondary text for marking edges or None
+            
+    def inbox(self, pt):
+        '''Checks if a point is within the box.
+
+        Parameters
+        ----------
+        pt : tuple
+            The x, y coordinates of the point.
+
+        Returns
+        -------
+        bool
+            True if the point is within the box, False otherwise.
+        '''
+        x, y = pt  
+        isbetween = between(self.top, self.bottom, y) and between(self.left, self.right, x)
+        return isbetween
+    
+    def update_val(self, value, cl=None):
+        '''Updates the value of the box.
+
+        Parameters
+        ----------
+        value : float
+            The new value.
+        cl : str, optional
+            The color of the box.
+        '''
+        self.ao.set_text(value)
+        if cl:
+            self.ao.set_c(cl)
+        else:
+            self.ao.set_c(self.cl)
+            
+    def show_secondary(self):
+        '''Shows the secondary text.'''
+        if self.astr:  # If there is a secondary set of text
+            self.astr.ao.set_c('green')
+
+    def clear_secondary(self):
+        '''Clears the secondary text.'''
+        if self.astr:  # If there is a secondary set of text
+            self.astr.ao.set_c('white')
+
+class plt_network:
+    '''Handles the plotting of a network.'''
+
+    def __init__(self, fn, image, out=None):
+        '''Initializes the PltNetwork.
+
+        Parameters
+        ----------
+        fn : function
+            The function to call to configure the network.
+        image : str
+            The path to the image file.
+        out : output, optional
+            The output for debugging.
+        '''
+        self.out = out  # Debug
+        img = plt.imread(image)
+        self.fig, self.ax = plt.subplots(figsize=self.sizefig(img))
+        boxes = fn(self.ax)
+        self.boxes = boxes
+        widgvis(self.fig)
+        self.ax.xaxis.set_visible(False)
+        self.ax.yaxis.set_visible(False)
+        self.ax.imshow(img)
+        self.fig.text(0.1, 0.9, 'Click in boxes to fill in values.')
+        self.glist = []  # Place to stash global things 
+        self.san = []  # Selected annotation
+        
+        self.cid = self.fig.canvas.mpl_connect('button_press_event', self.onclick)
+        self.axreveal = plt.axes([0.55, 0.02, 0.15, 0.075])  # Use [left, bottom, width, height]
+        self.axhide = plt.axes([0.76, 0.02, 0.15, 0.075])
+        self.breveal = Button(self.axreveal, 'Reveal All')
+        self.breveal.on_clicked(self.reveal_values)
+        self.bhide = Button(self.axhide, 'Hide All')
+        self.bhide.on_clicked(self.hide_values)
+
+    def sizefig(self, img):
+        '''Determines the size of the figure based on the image.
+
+        Parameters
+        ----------
+        img : ndarray
+            The image data.
+
+        Returns
+        -------
+        tuple
+            The size of the figure.
+        '''
+        iy, ix, iz = np.shape(img)
+        if 10 / 5 < ix / iy:  # If x is the limiting size
+            figx = 10
+            figy = figx * iy / ix
+        else:
+            figy = 5
+            figx = figy * ix / iy
+        return (figx, figy)
+       
+    def updateval(self, event):
+        '''Updates the value.
+
+        Parameters
+        ----------
+        event : str
+            The new value.
+        '''
+        box = self.san[0]
+        num_format = re.compile(r'[+-]?\d+(?:\.\d+)?')
+        isnumber = re.match(num_format, event)
+        if not isnumber:
+            box.update_val('?', 'red')
+        else:
+            newval = int(float(event)) if int(float(event)) == float(event) else float(event)
+            newval = round(newval, 2)
+            if newval == box.value:
+                box.show_secondary()
+                box.update_val(round(newval, 2))
+            else:
+                box.update_val(round(newval, 2), 'red')
+                box.clear_secondary()
+        self.glist[0].remove()
+        self.glist.clear()
+        self.san.clear()
+
+    def onclick(self, event):
+        '''Collects all clicks within diagram and dispatches.
+
+        Parameters
+        ----------
+        event : matplotlib.backend_bases.MouseEvent
+            The mouse event.
+        '''
+        if len(self.san) != 0:  # Already waiting for new value
+            return
+        inbox, box = inboxes((event.xdata, event.ydata), self.boxes)
+        if inbox:
+            self.san.append(box)
+            graphBox = self.fig.add_axes([0.225, 0.02, 0.2, 0.075])  # Use [left, bottom, width, height]
+            txtBox = TextBox(graphBox, 'newvalue: ')
+            txtBox.on_submit(self.updateval) 
+            self.glist.append(graphBox)
+            self.glist.append(txtBox)
+        return
+
+
+    def reveal_values(self, event):
+        '''Reveals the values.
+
+        Parameters
+        ----------
+        event : matplotlib.backend_bases.MouseEvent
+            The mouse event.
+        '''
+        for b in self.boxes:
+            b.update_val(b.value)
+            b.show_secondary()
+        plt.draw()
+
+
+    def hide_values(self, event):
+        '''Hides the values.
+
+        Parameters
+        ----------
+        event : matplotlib.backend_bases.MouseEvent
+            The mouse event.
+        '''
+        for b in self.boxes:
+            b.update_val('?')
+            b.clear_secondary()
+        plt.draw()
+    
+
+def config_nw0(ax):
+    '''Configures the network 0.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The axes object.
+
+    Returns
+    -------
+    list
+        A list of boxes.
+    '''
+    w = 3
+    a = 2 + 3 * w
+    J = a ** 2
+
+    pass
+    dJ_dJ = 1
+    dJ_da = 2 * a
+    dJ_da = dJ_dJ * dJ_da
+    da_dw = 3
+    dJ_dw = dJ_da * da_dw
+
+    box1 = ABox(ax, round(a, 2), 307, 140, 352, 100, (315, 128), 'blue', None)
+    box2 = ABox(ax, round(J, 2), 581, 138, 624, 100, (589, 128), 'blue', None)
+
+    dJ_da_a = astring(ax, r'$\frac{\partial J}{\partial a}=$' + f'{dJ_da}', (291, 186), 'green')
+    box3 = ABox(ax, round(dJ_da, 2), 545, 417, 588, 380, (553, 407), 'green', dJ_da_a)
+
+    dJ_dw_a = astring(ax, r'$\frac{\partial J}{\partial w}=$' + f'{dJ_dw}', (60, 186), 'green')
+    box4 = ABox(ax, round(da_dw, 2), 195, 421, 237, 380, (203, 411), 'green', None)
+    box5 = ABox(ax, round(dJ_dw, 2), 265, 515, 310, 475, (273, 505), 'green', dJ_dw_a)
+
+    boxes = [box1, box2, box3, box4, box5]
+
+    return boxes
+
+
+def config_nw1(ax):
+    '''Configures the network 1.
+
+    Parameters
+    ----------
+    ax : matplotlib.axes.Axes
+        The axes object.
+
+    Returns
+    -------
+    list
+        A list of boxes.
+    '''
+    x = 2
+    w = -2
+    b = 8
+    y = 1
+
+    c = w * x
+    a = c + b
+    d = a - y
+    J = d ** 2 / 2
+
+    pass
+    dJ_dJ = 1
+    dJ_dd = 2 * d / 2
+    dJ_dd = dJ_dJ * dJ_dd
+    dd_da = 1
+    dJ_da = dJ_dd * dd_da
+    da_db = 1
+    dJ_db = dJ_da * da_db
+    da_dc = 1
+    dJ_dc = dJ_da * da_dc
+    dc_dw = x
+    dJ_dw = dJ_dc * dc_dw
+
+    box1 = ABox(ax, round(c, 2), 330, 162, 382, 114, (338, 150), 'blue', None)
+    box2 = ABox(ax, round(a, 2), 636, 162, 688, 114, (644, 150), 'blue', None)
+    box3 = ABox(ax, round(d, 2), 964, 162, 1015, 114, (972, 150), 'blue', None)
+    box4 = ABox(ax, round(J, 2), 1266, 162, 1315, 114, (1274, 150), 'blue', None)
+
+    dJ_dd_a = astring(ax, r'$\frac{\partial J}{\partial d}=$' + f'{dJ_dd}', (967, 208), 'green')
+    box5 = ABox(ax, round(dJ_dd, 2), 1222, 488, 1275, 441, (1230, 478), 'green', dJ_dd_a)
+
+    dJ_da_a = astring(ax, r'$\frac{\partial J}{\partial a}=$' + f'{dJ_da}', (615, 208), 'green')
+    box6 = ABox(ax, round(dd_da, 2), 900, 383, 951, 333, (908, 373), 'green', None)
+    box7 = ABox(ax, round(dJ_da, 2), 988, 483, 1037, 441, (996, 473), 'green', dJ_da_a)
+
+    dJ_dc_a = astring(ax, r'$\frac{\partial J}{\partial c}=$' + f'{dJ_dc}', (337, 208), 'green')
+    box8 = ABox(ax, round(da_dc, 2), 570, 380, 620, 333, (578, 370), 'green', None)
+    box9 = ABox(ax, round(dJ_dc, 2), 638, 467, 688, 419, (646, 457), 'green', dJ_dc_a)
+
+    dJ_db_a = astring(ax, r'$\frac{\partial J}{\partial b}=$' + f'{dJ_dc}', (474, 252), 'green')
+    box10 = ABox(ax, round(da_db, 2), 563, 582, 615, 533, (571, 572), 'green', None)
+    box11 = ABox(ax, round(dJ_db, 2), 630, 677, 684, 630, (638, 667), 'green', dJ_db_a)
+
+    dJ_dw_a = astring(ax, r'$\frac{\partial J}{\partial w}=$' + f'{dJ_dw}', (60, 208), 'green')
+    box12 = ABox(ax, round(dc_dw, 2), 191, 379, 341, 332, (199, 369), 'green', None)
+    box13 = ABox(ax, round(dJ_dw, 2), 266, 495, 319, 448, (274, 485), 'green', dJ_dw_a)
+
+    boxes = [box1, box2, box3, box4, box5, box6, box7, box8, box9, box10, box11, box12, box13]
+
+    return boxes
